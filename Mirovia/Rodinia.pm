35685f29440df670435468e366a29b86f9d276f4
Reid Netterville III <reidiiius@gmail.com>
2022-02-12 23:56:20 -0600

    1644731780

--- Mirovia/Rodinia.pm
+++ Mirovia/Rodinia.pm
@@ -162,5 +162,74 @@ sub dashboard {
   print "\n\n";
 }
 
+sub entryway {
+
+  if (@_) {
+    my $tune  = 'beadgcf';           # default tuning
+    my $clef  = validate( $_[0] );
+    my $alert = 'initialization';
+
+    unless ($clef) {
+      $tune = shift;                 # change tuning
+
+      unless ( boundary $tune ) {
+        $alert = caution($tune);
+        print "\n$alert\n";
+        return 0;
+      }
+    }
+
+    if ( $tune eq 'search' and defined $_[0] ) {
+      my $span = CURB * 6;
+
+      if ( length $_[0] <= $span ) {
+        &kleenex;
+        return 0;
+      }
+      else {
+        $alert = caution( $_[0], $span );
+        print "\n$alert\n";
+        return 0;
+      }
+    }
+    elsif ( @_ and $_[0] eq 'gamut' ) {
+      my @arks = Gondwana::keynotes;
+
+      print "\n";
+      for (@arks) {
+        vestibule( $tune, $_ );
+        print "\n";
+      }
+      return 0;
+    }
+    elsif (@_) {
+      print "\n";
+
+      for (@_) {
+        if ( boundary $_ ) {
+          vestibule( $tune, $_ );
+          print "\n";
+        }
+        else {
+          $alert = caution($_);
+          print "$alert\n";
+        }
+      }
+    }
+    else {
+      my $snip = penlight( 'Example:', 96 );
+      my $opus = "perl -w $0";
+      print "\n\t$snip\n";
+      print "\t\t$opus viola n0 j36\n\n";
+      print "\t\t$opus viola: n0 j36\n\n";
+      print "\t\t$opus search '^[jk]\\d([jk]\\d)?\$'\n\n";
+      print "\t\t$opus guitar gamut\n\n";
+    }
+  }
+  else {
+    dashboard;
+  }
+}
+
 1;
 
